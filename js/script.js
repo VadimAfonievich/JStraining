f033ec5a0054b4e039757282c4f813288bd972ba
Vadim <0711aiova@gmail.com>
2020-12-24 21:57:05 +0300

    practice ch.1

--- js/script.js
+++ js/script.js
@@ -1,85 +1,21 @@
 "use strict";
 
-const category = 'toys';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let price = '25$';
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его по 10 бальной шкале?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = +prompt('На сколько оцените его по 10 бальной шкале?', '');
 
-console.log(`https://someurl.com/${category}/5/${price}/6/`);
-
-const user = prompt('What is your name?' + '');
-alert(`Hello, ${user}`);
-
-
-
-/*
-//Lesson 7. Общение с пользователем.
-
-alert('Hello!');
-
-const result = confirm('Are you here?!');
-console.log(result);
-
-let answer = prompt('Вам  есть 18?', '18');
-console.log(answer + 5);
-
-const answers = [];
-
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-document.write(answers);
-*/
-
-
-
-/* lesson 2-6
-let number = 5;
-const leftBorderWidth = 1;
-
-number = 10;
-console.log(number);
-
-
-const obj = {
-
-    name: "Alex",
-    age: 25,
-    isMarried: false,
-    vife: 'Anastasiya'    
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj.name);
-console.log(obj.age);
-console.log(obj.isMarried);
-console.log(obj.vife);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-console.log(arr[1]);
-
-
-let number1 = 4.6;
-
-console.log(-4/0);
-console.log('string' * 9);
-
-const bool = false;
-
-console.log(something);
-
-{
-    var result = 50;
-}
-console.log(result);
-*/
-
-
-/*
-var name = 'Ivan';
-
-    name = 'Alez';
-
-    console.log(name);
-*/
-
-
+console.log(personalMovieDB);
